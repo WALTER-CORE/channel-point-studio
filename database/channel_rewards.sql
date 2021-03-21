ce0a59453929bece7abf3f834a644686af023521
jongjam <jongjam64@gmail.com>
2021-03-21 01:48:36 -0700

    connected api and sql

--- database/channel_rewards.sql
+++ database/channel_rewards.sql
@@ -6,3 +6,8 @@ tier int(3),
 cost int,
 redemption_count int
 );
+
+INSERT INTO channel_rewards (reward_id, name, user_id,
+                                tier, cost, redemption_count)
+    VALUES ('1b7592c0-c6a0-4d2f-aaef-04902cc474fb', 'Drink Water',
+                'billy', '1', '500', '67');
